325602c6e63350dab10b9df893aac9e2e2639e0f
Atrem <zerlingtail@gmail.com>
2025-03-05 12:21:23 +0200

    Добавьте файлы проекта.

---
+++ Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            float b = float.Parse(Console.ReadLine());
+
+            
+            Console.Write($"Какую операцию провести над числами?\n\t{a} \"операция\" {b}\n");
+            Console.WriteLine("1. Сложение\n2. Вычитание\n3. Умножение\n4. Деление");
+            int choise = Int32.Parse(Console.ReadLine());
+            switch(choise)
+            {
+                case 1:
+                    Console.WriteLine($"Сложение: {a} + {b} = {a + b}");
+                    break;
+                case 2:
+                    Console.WriteLine($"Сложение: {a} - {b} = {a - b}");
+                    break;
+                case 3:
+                    Console.WriteLine($"Умножение: {a} * {b} = {a * b}");
+                    break;
+                case 4:
+                    if(b == 0)
+                    {
+                        Console.WriteLine("Второе число равно нулю! На ноль делить нельзя!");
+                        break;
+                    }
+                    Console.WriteLine($"Деление: {a} / {b} = {a / b}");
+                    break;
+                default:
+                    Console.WriteLine("Неправильный пункт меню!");
+                    break;
+            }
+        }
+    }
+}
